76d826f5a036b21d5e0aea58c50981eea92d6608
Anders Brownworth <anders-github@evantide.com>
2024-10-19 09:37:53 -0400

    fix images and remove old BNK references

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     postgres: {
 	host: process.env.PGHOST || 'localhost',
-	database: process.env.PGDATABASE || 'bnk_dev',
-	user: process.env.PGUSER || 'bnk',
+	database: process.env.PGDATABASE || 'allowance_app_dev',
+	user: process.env.PGUSER || 'allowance_app',
 	port: process.env.PGPORT || 5432,
 	password: process.env.PGPASSWORD || 'supersecretpassword',
 	ssl: false,
